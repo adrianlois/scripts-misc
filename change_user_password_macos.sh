9bc7a9a8ee02d070d699724bf605015515245d76
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2020-10-30 21:28:41 +0100

    change_user_password_macos.sh

---
+++ change_user_password_macos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+unset HISTFILE
+
+USERNAME="adrian"
+OLDPASS="Abc123"
+NEWPASS="123Abc"
+
+/usr/bin/dscl . -passwd /Users/"${USERNAME}" "${OLDPASS}" "${NEWPASS}"
+security set-keychain-password -o "${OLDPASS}" -p "${NEWPASS}" /Users/"${USERNAME}"/Library/Keychains/login.keychain
+
+history -c
