4034d5127cf0349e2d31d0de26bba2863ee59c95
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-03-23 21:27:44 +0100

    NoLock.ps1

---
+++ NoLock.ps1
@@ -0,0 +1,10 @@
+# Prevents computer lockup by simulating Ctrl key presses every 3 minutes
+
+function NoLock {
+	Write-Host "..."
+	for (;;) {
+		[void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
+		[System.Windows.Forms.SendKeys]::SendWait("^")
+		Start-Sleep 180
+	}
+}
