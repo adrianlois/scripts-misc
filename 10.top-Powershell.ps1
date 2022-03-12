dc97074be584bf4aca0e0770f7d362809ff9e9e6
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2022-03-12 21:58:15 +0100

    10.top-Powershell.ps1

--- 10.top-Powershell.ps1
+++ 10.top-Powershell.ps1
@@ -1,3 +1,8 @@
+<#
+    .DESCRIPTION
+	This function for PowerShell acts as an equivalent of the Linux top command.
+#>
+
 function top {
 	while (1) { 
 		Get-Process | Sort -Descending cpu | Select-Object -First 30; Start-Sleep -Seconds 2; Clear-Host; 
