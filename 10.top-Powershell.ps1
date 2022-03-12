9235d3ad71dad511aa58bb55d661a7d46117fc2d
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2022-03-12 21:52:33 +0100

    10.top-Powershell.ps1

---
+++ 10.top-Powershell.ps1
@@ -0,0 +1,7 @@
+function top {
+	while (1) { 
+		Get-Process | Sort -Descending cpu | Select-Object -First 30; Start-Sleep -Seconds 2; Clear-Host; 
+		Write-Host "Handles  NPM(K)    PM(K)      WS(K) VM(M)   CPU(s)     Id ProcessName"; 
+		Write-Host "-------  ------    -----      ----- -----   ------     -- -----------"
+	}
+}
