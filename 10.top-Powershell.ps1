df5754c168a9cf837a2df795ade4d05e6943aaa1
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2022-03-12 22:52:08 +0100

    10.top-PowerShell.ps1

--- 10.top-Powershell.ps1
+++ 10.top-Powershell.ps1
@@ -1,11 +1,12 @@
 <#
     .DESCRIPTION
 	This function for PowerShell acts as an equivalent of the Linux top command.
+	Displays the first 40 CPU consuming processes in descending order.
 #>
 
 function top {
 	while (1) { 
-		Get-Process | Sort -Descending cpu | Select-Object -First 30; Start-Sleep -Seconds 2; Clear-Host; 
+		Get-Process | Sort -Descending cpu | Select-Object -First 40; Start-Sleep -Seconds 2; Clear-Host; 
 		Write-Host "Handles  NPM(K)    PM(K)      WS(K) VM(M)   CPU(s)     Id ProcessName"; 
 		Write-Host "-------  ------    -----      ----- -----   ------     -- -----------"
 	}
