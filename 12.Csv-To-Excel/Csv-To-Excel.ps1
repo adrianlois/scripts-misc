d6a3a8ae7207071dfbd865485b54cd761eebabdf
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2022-05-03 12:56:15 +0200

    12.Csv-To-Excel

---
+++ 12.Csv-To-Excel/Csv-To-Excel.ps1
@@ -0,0 +1,26 @@
+# https://www.powershellgallery.com/packages/ImportExcel/7.5.0
+# Install-Module -Name ImportExcel
+
+Function Csv-To-Excel {
+    param (
+        [Parameter(Mandatory=$True)]
+        [string]$CsvPath,
+        [Parameter(Mandatory=$True)]
+        [string]$XlsxFile
+    )
+
+    $CsvFiles = (Get-ChildItem -Path $CsvPath -Filter "*.csv").Name
+
+    ForEach ($CsvFile in $CsvFiles) {
+
+		$WorkSheetname = $CsvFile.Substring(0,$CsvFile.Length -4)
+
+        Import-Csv -Path "$CsvPath\$CsvFile" | Export-Excel -Path "$XlsxFile" -WorkSheetname "$WorkSheetname" `
+        -NoNumberConversion IPv4Address	-AutoSize -BoldTopRow -AutoFilter -ConditionalText $(
+            New-ConditionalText True -BackgroundColor LightGreen -ConditionalTextColor DarkGreen
+            New-ConditionalText False -BackgroundColor Yellow -ConditionalTextColor Red
+        )
+    }
+}
+
+Csv-To-Excel -CsvPath "C:\path_impot\csv_files" -XlsxFile "C:\path_export\output.xlsx"
---
+++ 12.Csv-To-Excel/README.md
@@ -0,0 +1,17 @@
+### Csv-To-Excel
+
+PowerShell module to import/export Excel spreadsheets, without Excel.
+
+### Import module Powershell: Import-Excel
+
+```
+Install-Module -Name ImportExcel
+```
+
+### Info about the Import-Excel module
+
+- Installation: https://www.powershellgallery.com/packages/ImportExcel/7.5.0 
+- cmdlet Import-Excel: https://www.powershellgallery.com/packages/ImportExcel/7.5.0/Content/Public%5CImport-Excel.ps1
+- cmdlet Export-Excel: https://www.powershellgallery.com/packages/ImportExcel/7.5.0/Content/Public%5CExport-Excel.ps1
+- Github: https://github.com/dfinke/ImportExcel
+
