51d4aa91da578acdfcf342d9a08d9990974830bf
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2023-02-27 22:24:02 +0100

    Update 17.SoundPlayer.ps1

--- 17.SoundPlayer.ps1
+++ 17.SoundPlayer.ps1
@@ -1,8 +1,11 @@
 $Audio = New-Object System.Media.SoundPlayer
 $Audio.SoundLocation = "C:\temp\media\sound.wav"
+
 # Play once
 $Audio.Play()
+
 # Play loop
 $Audio.PlayLooping()
+
 # Stop play
 $Audio.Stop()
