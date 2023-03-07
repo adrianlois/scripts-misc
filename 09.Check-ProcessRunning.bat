a18e7cd5a2c73ac04fdfe39b6a46450540e846bc
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2023-03-07 17:06:13 +0100

    update scripts

---
+++ 09.Check-ProcessRunning.bat
@@ -0,0 +1,7 @@
+set WPROCESS=Notepad.exe
+tasklist /fi "ImageName eq %WPROCESS%" /fo csv 2>NUL | findstr /I "%WPROCESS%">NUL
+if "%ERRORLEVEL%"=="0" (
+echo Process %WPROCESS% - Application is running
+) else (
+echo Process %WPROCESS% - Application is not running
+)
