f4d371720f15c4eaaf07b87fc90f19c1f2dfe821
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2023-03-16 17:17:21 +0100

    Update 24.Get-FSMO-Roles-DCs.ps1

--- 24.Get-FSMO-Roles-DCs.ps1
+++ 24.Get-FSMO-Roles-DCs.ps1
@@ -1,4 +1,4 @@
 # Info: https://www.zonasystem.com/2018/12/transferir-roles-fsmo-windows-server-y-purgado-de-servicios-del-dominio.html
 
-Get-ADForest | Select-Object DomainNamingMaster, SchemaMaster | Format-List
 Get-ADDomain | Select-Object InfrastructureMaster, RIDMaster, PDCEmulator | Format-List
+Get-ADForest | Select-Object DomainNamingMaster, SchemaMaster | Format-List
