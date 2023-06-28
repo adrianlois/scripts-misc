049991f9148adc3bda364bfc3a176b835d813d9d
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2023-06-28 12:40:27 +0200

    Update 22.Get-USB-Installed.ps1

--- 22.Get-USB-Installed.ps1
+++ 22.Get-USB-Installed.ps1
@@ -1,3 +1,11 @@
-Get-WmiObject -Class Win32_PNPEntity | `
-Select-Object -Property PNPDeviceID, Caption, Service | `
-Where-Object {$_.PNPDeviceID -like "USB*"}
+# Option 1 (PS)
+Get-WmiObject -Class Win32_PNPEntity | Select-Object -Property PNPDeviceID, Caption, Service | Where-Object {$_.PNPDeviceID -like "USB*"}
+
+# Option 2 (PS)
+Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -match '^USB' } | Select-Object Name, Description, HardwareID | Format-List
+
+# Option 1 (CMD)
+wmic path win32_pnpentity where "Present='True' AND PNPClass='USB'" get Name,Description,HardwareID
+
+# Option 2 (CMD)
+pnputil /enum-devices /connected /class USB
