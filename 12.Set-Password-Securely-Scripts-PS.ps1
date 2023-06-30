095d9b960ad73352be50618f7e589d3b06071707
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2023-06-30 15:40:04 +0200

    Update 12.Set-Password-Securely-Scripts-PS.ps1

--- 12.Set-Password-Securely-Scripts-PS.ps1
+++ 12.Set-Password-Securely-Scripts-PS.ps1
@@ -1,7 +1,7 @@
 <#
-Create a file with the encrypted password:
-One way to avoid typing a password in clear text in a ps1 script is to generate a file containing the AES256 hash corresponding to the encrypted password string. 
-This would be a more secure way to parameterize credentials or passwords without the need to display them in clear text and at all points maintain the confidentiality of the password in PowerShell script automation.
+    Create a file with the encrypted password:
+    One way to avoid typing a password in clear text in a ps1 script is to generate a file containing the AES256 hash corresponding to the encrypted password string. 
+    This would be a more secure way to parameterize credentials or passwords without the need to display them in clear text and at all points maintain the confidentiality of the password in PowerShell script automation.
 #>
 
 # We generate the encrypted file with the password ("MyPassword")
