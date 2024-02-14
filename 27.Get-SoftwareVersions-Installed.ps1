551e08ef27182216cddc58594c34b94684f95d5d
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2024-02-14 20:53:54 +0100

    Update 27.Get-SoftwareVersions-Installed.ps1

--- 27.Get-SoftwareVersions-Installed.ps1
+++ 27.Get-SoftwareVersions-Installed.ps1
@@ -1,5 +1,4 @@
 # The fastest and most effective way to get a list of installed software and its versions on a computer on x86 and x64 architectures. In one line.
-
 Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | ? {![string]::IsNullOrWhiteSpace($_.DisplayName) } | Select-Object DisplayName, DisplayVersion
 
 # Another alternative that is not as effective
