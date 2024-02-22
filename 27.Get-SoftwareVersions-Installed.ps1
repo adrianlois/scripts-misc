f393a4bfc4a0330a37231fc99d048a448ff99f6f
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2024-02-22 21:48:17 +0100

    Update 27.Get-SoftwareVersions-Installed.ps1

--- 27.Get-SoftwareVersions-Installed.ps1
+++ 27.Get-SoftwareVersions-Installed.ps1
@@ -3,7 +3,9 @@ Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, `
                  HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, `
                  HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*, `
                  HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | `
-                 ? {![string]::IsNullOrWhiteSpace($_.DisplayName) } | Select-Object DisplayName, DisplayVersion
+                 ? {![string]::IsNullOrWhiteSpace($_.DisplayName) } | `
+                 Select-Object DisplayName, DisplayVersion, InstallDate | Sort-Object DisplayName -Unique
 
 # Another alternative that is not as effective
 Get-WmiObject -Class Win32_Product | Select-Object Name, Version
+Get-WmiObject -Query "SELECT * FROM Win32_Product" | Select-Object Name, Version, Vendor, InstallDate
