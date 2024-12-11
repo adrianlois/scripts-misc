70736e8df9f23b782f7decf72663c14d502ac32c
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2024-12-11 11:11:40 +0100

    Update 29.SSH-Tunel-WebSOCKS.ps1

--- 29.SSH-Tunel-WebSOCKS.ps1
+++ 29.SSH-Tunel-WebSOCKS.ps1
@@ -2,7 +2,6 @@
     # -i: Path to the private key for public key authentication on the server.
     # -N: Prevents remote command execution and does not start an interactive shell or remote session. Useful for port forwarding only.
 
-
 # Using plink (https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html)
 echo "`n" | plink -D 9090 <USER>@<REMOTE_IP> -P <REMOTE_PORT> -N -i "C:\<PATH>\idkey.ppk"
 
